dbf3ee872d8131b71b4c5d4b1bc2cf7e46043538
Adrian Jansen Quindeler <adrian.quindeler@comunicacontactcenter.local>
2024-03-15 11:56:07 -0300

    adicionando classes: instantiation e DTO

--- workshop_mongodb/src/main/java/com/example/workshopmongo/resources/UserResource.java
+++ workshop_mongodb/src/main/java/com/example/workshopmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.example.workshopmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.workshopmongo.domain.User;
+import com.example.workshopmongo.dto.UserDTO;
 import com.example.workshopmongo.services.UserService;
 
 @RestController
@@ -19,8 +21,9 @@ public class UserResource {
 	private UserService service;
 
 	@RequestMapping(method = RequestMethod.GET)
-	private ResponseEntity<List<User>> findAll(){
+	private ResponseEntity<List<UserDTO>> findAll(){
 		List<User> users = service.findAll();
-		return ResponseEntity.ok().body(users);
+		List<UserDTO> userDTO = users.stream().map(user -> new UserDTO(user)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(userDTO);
 	}
 }
