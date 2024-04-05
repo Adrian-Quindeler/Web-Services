4a7b5994b1be23789a5570ebc1d7253899c50b8a
AdrianJQuindeler <adrianquindeler@gmail.com>
2024-04-05 11:56:36 -0300

    Consulta por título do post

--- workshop_mongodb/src/main/java/com/example/workshopmongo/services/PostService.java
+++ workshop_mongodb/src/main/java/com/example/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.example.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,5 +20,9 @@ public class PostService {
 	public Post findById(String id) {
 		Optional<Post> post = repository.findById(id);
 		return post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public List<Post> findByTitle(String title){
+		return repository.searchTitle(title);
 	}
 }
